7a5b4b672cc750f164673c2245f261c7620fe7e9
Alexander Nicholson <alexn400@gmail.com>
2021-01-23 10:15:17 +1300

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useSelector } from 'react-redux';
+import Button from './components/Button';
+import { toggle } from './redux/stateSlice/StateSlice';
+import { RootState, useAppDispatch } from './redux/store';
 
-function App() {
+const App = () => {
+  const dispatch = useAppDispatch();
+
+  const selector = (state: RootState) => state.state;
+
+  const store = useSelector(selector);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Button onClick={() => dispatch(toggle())} />
+      <br />
+      {store.toggle ? 'On' : 'Off'}
     </div>
   );
-}
+};
 
 export default App;
